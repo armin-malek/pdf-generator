d8569c5b3ea61c9537cef53bcb94a96ffa4141fd
arminmalek <armine.s8888@gmail.com>
2025-02-10 09:50:27 +0330

    !

--- server.js
+++ server.js
@@ -30,12 +30,49 @@ async function launchBrowser() {
   console.log("Launching browser...");
   browserPromise = puppeteer.launch({
     headless: NODE_ENV == "PRODUCTION" ? true : false,
-    // executablePath:
-    //   NODE_ENV == "PRODUCTION"
-    //     ? undefined
-    //     : "C:\\Users\\Armin\\AppData\\Local\\Chromium\\Application\\chrome.exe",
-    defaultViewport: { width: 1920, height: 1080 },
-    args: ["--no-sandbox"],
+    // headless: true,
+    executablePath:
+      NODE_ENV == "PRODUCTION"
+        ? undefined
+        : "C:\\Users\\Armin\\AppData\\Local\\Chromium\\Application\\chrome.exe",
+    defaultViewport: { width: 640, height: 480 },
+    args: [
+      "--autoplay-policy=user-gesture-required",
+      "--disable-background-networking",
+      "--disable-background-timer-throttling",
+      "--disable-backgrounding-occluded-windows",
+      "--disable-breakpad",
+      "--disable-client-side-phishing-detection",
+      "--disable-component-update",
+      "--disable-default-apps",
+      "--disable-dev-shm-usage",
+      "--disable-domain-reliability",
+      "--disable-extensions",
+      "--disable-features=AudioServiceOutOfProcess",
+      "--disable-hang-monitor",
+      "--disable-ipc-flooding-protection",
+      "--disable-notifications",
+      "--disable-offer-store-unmasked-wallet-cards",
+      "--disable-popup-blocking",
+      "--disable-print-preview",
+      "--disable-prompt-on-repost",
+      "--disable-renderer-backgrounding",
+      "--disable-setuid-sandbox",
+      "--disable-speech-api",
+      "--disable-sync",
+      "--hide-scrollbars",
+      "--ignore-gpu-blacklist",
+      "--metrics-recording-only",
+      "--mute-audio",
+      "--no-default-browser-check",
+      "--no-first-run",
+      "--no-pings",
+      "--no-sandbox",
+      "--no-zygote",
+      "--password-store=basic",
+      "--use-gl=swiftshader",
+      "--use-mock-keychain",
+    ],
   });
 
   const browser = await browserPromise;
